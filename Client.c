4898788b6007c096aa8da084c8c96b5f2408628a
praveecyber21 <praveencyber21@gmail.com>
2024-08-08 17:41:56 +0530

    feat: File sending code added

--- Client.c
+++ Client.c
@@ -1,8 +1,44 @@
 //  File transfer using tcp - Client program
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define SIZE 1024
+
+void receive_file(int sockfd)
+{
+    FILE *fp = fopen("receive.txt", "a");
+
+    char buffer[SIZE];
+    // recv(sockfd, buffer, sizeof(buffer), 0);
+
+    // if (sizeof(buffer) != 0)
+    // {
+    //     printf("%s\n", buffer);
+    //     exit(EXIT_FAILURE);
+    // }
+    ssize_t datareceived; // Stores the number bytes received from the socket
+    while ((datareceived = recv(sockfd, buffer, sizeof(buffer), 0)) > 0)
+    {
+
+        buffer[datareceived] = '\0';
+        fprintf(fp, "%s", buffer);
+        bzero(buffer, SIZE);
+    }
+    if (datareceived < 0)
+    {
+        perror("[x] Error receving data");
+    }
+
+    printf("[\u2714] File receive successfully\n");
+
+    fclose(fp);
+    close(sockfd);
+}
 
 int main(int argc, char const *argv[])
 {
@@ -12,11 +48,12 @@ int main(int argc, char const *argv[])
 
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
-        perror("Socket error");
+        perror("[x] Socket error");
+        exit(EXIT_FAILURE);
     }
     else
     {
-        printf("Socket created\n");
+        printf("[\u2714] Socket created\n");
     }
     client_address.sin_family = AF_INET;
     client_address.sin_port = htons(9999);
@@ -24,12 +61,23 @@ int main(int argc, char const *argv[])
 
     if (connect(sockfd, (struct sockaddr *)&client_address, addrlen) < 0)
     {
-        perror("Server is not ready");
+        perror("[x] Server is not ready");
+        exit(EXIT_FAILURE);
     }
     else
     {
-        printf("Successfully connected with server\n");
+        printf("[\u2714] Successfully connected with server\n");
     }
+
+    // // Get the filename from the user
+    // char filename[25];
+
+    // printf("Enter file  name: ");
+    // scanf(" [%^\n]s", filename);
+
+    // receive_file(sockfd, filename);
+
+    receive_file(sockfd);
 
     close(sockfd);
 
--- Server.c
+++ Server.c
@@ -1,8 +1,42 @@
 //  File transfer using tcp - Server program
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define SIZE 1024
+void send_file(int new_sockfd)
+{
+    FILE *fp = fopen("send.txt", "r");
+
+    // if (fp == NULL)
+    // {
+    //     perror("File error");
+    //     send(new_sockfd, "File error", SIZE, 0);
+
+    //     exit(EXIT_FAILURE);
+    // }
+
+    char buffer[SIZE];
+    while (1)
+    {
+        if (fgets(buffer, sizeof(buffer), fp) != NULL)
+        {
+            send(new_sockfd, buffer, strlen(buffer), 0);
+        }
+        else
+        {
+            break;
+        }
+        bzero(buffer, SIZE);
+    }
+
+    printf("[\u2714] File sended successfully\n");
+    fclose(fp);
+}
 
 int main(int argc, char const *argv[])
 {
@@ -13,11 +47,12 @@ int main(int argc, char const *argv[])
 
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
-        perror("Socket Error");
+        perror("[x] Socket Error");
+        exit(EXIT_FAILURE);
     }
     else
     {
-        printf("Socket Created\n");
+        printf("[\u2714] Socket Created\n");
     }
 
     server_socket.sin_family = AF_INET;
@@ -26,21 +61,30 @@ int main(int argc, char const *argv[])
 
     if (bind(sockfd, (struct sockaddr *)&server_socket, addrlen) < 0)
     {
-        perror("Bind error");
+        perror("[x] Bind error");
+        exit(EXIT_FAILURE);
     }
 
     if (listen(sockfd, 3) < 0)
     {
-        perror("Listen error");
+        perror("[x] Listen error");
+        exit(EXIT_FAILURE);
     }
 
-    printf("Server is listening...\n");
+    printf("[...] Server is listening...\n");
 
+    // Accept a new connection
     int new_sockfd;
     new_sockfd = accept(sockfd, (struct sockaddr *)&server_socket, &addrlen);
 
-    char buffer[50] = "Message from server";
-    send(new_sockfd, buffer, sizeof(buffer), 0);
+    // Receive a filename from a client
+    // char buffer[25];
+
+    // recv(new_sockfd, buffer, sizeof(buffer), 0);
+
+    // send_file(new_sockfd, buffer);
+
+    send_file(new_sockfd);
 
     close(sockfd);
     close(new_sockfd);
